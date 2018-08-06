b1e0634dbcd33b853e266e71de6e475c345191aa
Winnie Xu <winniexu@Winnies-MacBook-Pro.local>
2018-08-05 23:38:50 -0400

    made jscript loop i=0

--- slideshow.js
+++ slideshow.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   const MAX_IMAGES = 3;
   var body = $('body');
   var images = ['url(Canada1.png)', 'url(Canada2.png)', 'url(Canada3.png)'];
-  var i = 1;
+  var i = 0;
 
   function cycle() {
     if (i == MAX_IMAGES) {
@@ -14,7 +14,5 @@ $(document).ready(function() {
     setTimeout(cycle, 5000);
   }
 
-  setTimeout(cycle, 5000);
-  body.css('backgroundImage', images[0]);
+  cycle();
 });
-
